a9bf1ab28ce3ba5cfd69e400034b1040da760c65
William Ashton <William@AshtonFam.org>
2020-08-26 22:21:17 -0400

    Add Todo.Cache, end of section 7.2

---
+++ test/todo/cache_test.exs
@@ -0,0 +1,23 @@
+defmodule Todo.CacheTest do
+  use ExUnit.Case, async: true
+
+  test "general usage" do
+    {:ok, cache} = Todo.Cache.start()
+
+    bob_pid = Todo.Cache.server_process(cache, "Bob's list")
+    assert is_pid(bob_pid)
+
+    assert Todo.Cache.server_process(cache, "Bob's list") == bob_pid
+    assert Todo.Cache.server_process(cache, "Alice's list") != bob_pid
+  end
+
+  test "to-do operations" do
+    {:ok, cache} = Todo.Cache.start()
+
+    alice = Todo.Cache.server_process(cache, "alice")
+    Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+    entries = Todo.Server.entries(alice, ~D[2018-12-19])
+
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+  end
+end
